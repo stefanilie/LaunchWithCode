ca9ffb970d38c02c0c8bb8a2744ed7f05f6d5e3d
Stefan Ilie <ilie.stefan92@yahoo.com>
2015-03-21 21:45:13 +0200

    add email form

--- src/joins/views.py
+++ src/joins/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-
+from .forms import EmailForm, JoinForm
+from .models import Join
 # Create your views here. 
 
-
 def home(request):
-	context = {}
+	'''form = EmailForm(request.POST or None)
+	if form.is_valid():
+		email=form.cleaned_data['email']
+		new_join, created = Join.objects.get_or_create(email=email)'''
+	form=JoinForm(request.POST or None)
+	if form.is_valid():
+		new_join=form.save(commit=False)
+		email = form.cleaned_data['email']
+		new_join_old, created = Join.objects.get_or_create(email=email)
+		#new_join.save()
+	context = {"form": form}
 	template = "home.html"
 	return render(request, template, context)
